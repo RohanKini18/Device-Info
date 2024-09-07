bcf7272e9a6bb82f98532bdeff50b06675a2d412
ROHAN KINI <rohankini.rk18@gmail.com>
2024-09-08 00:50:11 +0530

    Update

--- main2.js
+++ main2.js
@@ -1,39 +1,58 @@
-initBattery()
-            function initBattery(){
-                const batteryLiquid = document.querySelector('.battery__liquid'),
-                      batteryStatus = document.querySelector('.battery__status'),
-                      batteryPercentage = document.querySelector('.battery__percentage')
-                
-                navigator.getBattery().then((batt) =>{
-                    updateBattery = () =>{
-                        
-                        let level = Math.floor(batt.level * 100)
-                        batteryPercentage.innerHTML ='Battery Percentage:'+' '+level+ '<b>%</b>'
-            
-                        
-                        batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`
-            
-                        
-                        if(level == 100){ 
-                            batteryStatus.innerHTML = 'Charging Status:'+' '+`<b>Full battery</b> <i class="ri-battery-2-fill green-color"></i>`
-                            batteryLiquid.style.height = '103%'
-                        }
-                        else if(level <= 20 &! batt.charging){ 
-                            batteryStatus.innerHTML = 'Charging Status:'+' '+`<b>Low battery</b> <i class="ri-plug-line animated-red"></i>`
-                        }
-                        else if(batt.charging){ 
-                            batteryStatus.innerHTML = 'Charging Status:'+' '+`<b>Charging...</b> <i class="ri-flashlight-line animated-green"></i>`
-                        }
-                        else{ 
-                            batteryStatus.innerHTML = 'Charging Status:'+' '+'<b>Charger Not connected</b>'
-                        }
-                        
-                      
-                    }
-                    updateBattery()
-            
-                   
-                    batt.addEventListener('chargingchange', () => {updateBattery()})
-                    batt.addEventListener('levelchange', () => {updateBattery()})
-                })
-            }
+var cell = document.getElementById('cell');
+var indicator = document.getElementById('indicator');
+ navigator.getBattery().then(function(battery) {
+   function updateAllBatteryInfo(){
+     updateChargeInfo();
+     updateLevelInfo();
+     updateChargingInfo();
+     updateDischargingInfo();
+   }
+   updateAllBatteryInfo();
+
+   battery.addEventListener('chargingchange', function(){
+     updateChargeInfo();
+   });
+   function updateChargeInfo(){
+      if(battery.charging){
+          cell.style.background="#06eeb0";
+          indicator.style.display="block";
+      }
+      else{
+        indicator.style.display="none";
+        if(battery.level<0.25){
+          cell.style.background="#e0821f";
+        }
+        else{
+        cell.style.background="#08c66b";
+        }
+      }
+   }
+
+   battery.addEventListener('levelchange', function(){
+     updateLevelInfo();
+   });
+   function updateLevelInfo(){
+     cell.style.height = battery.level * 100 + "%";
+     cell.innerHTML = "<br/>"+Math.round(battery.level * 100) + "%";
+     if(battery.level<0.15){
+       cell.style.background="#e0821f";
+     }
+   }
+
+   battery.addEventListener('chargingtimechange', function(){
+     updateChargingInfo();
+   });
+   function updateChargingInfo(){
+     console.log("Battery charging time: "
+                  + battery.chargingTime + " seconds");
+   }
+
+   battery.addEventListener('dischargingtimechange', function(){
+     updateDischargingInfo();
+   });
+   function updateDischargingInfo(){
+     console.log("Battery discharging time: "
+                  + battery.dischargingTime + " seconds");
+   }
+
+ });
